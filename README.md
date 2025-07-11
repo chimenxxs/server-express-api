9cd02e971dd529dc03522f095d4d8b4097ee0f2a
thielli chimenes <thielli.chimenes@estudante.ifms.edu.br>
2025-07-10 23:19:08 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+# Projeto Node.js com Express – API de Cliente e Produto
+
+Este projeto foi desenvolvido como parte da disciplina de LP3 no IFMS. Ele simula uma API RESTful utilizando Node.js, Express e PostgreSQL.
+
+## Funcionalidades
+
+- CRUD completo para **clientes** e **produtos**:
+  - GET
+  - POST
+  - PUT
+  - DELETE
+
+- Testes realizados com o Postman (5 por rota)
+
+## Como rodar o projeto
+
+1. Clone o repositório
+2. Instale as dependências com `npm install`
+3. Configure o arquivo `.env` com as variáveis de conexão com o banco
+4. Rode o servidor com `node app.js`
+
+
+##  aluna :
+
+Thielli de Arruda Chimenes – IFMS 5° semestre 
+
+
---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Servidor back-end utilizando o módulo Express que cria uma API Rest e realiza
+ * as operações de CRUD, envolvendo os métodos HTTP (GET, POST, PUT e DELETE)
+ */
+
+// Importar o módulo Express
+const express = require('express');
+
+// criação do aplicativo servidor
+const app = express();
+
+require('dotenv').config();
+
+// configuração de acesso ao servidor
+const hostname = process.env.APP_HOST;
+const port = process.env.APP_PORT;
+
+
+
+app.use(express.json());
+
+
+// importar as configurações de rotas
+const clienteRotas = require('./routes/cliente');
+const produtoRotas = require('./routes/produto');
+
+// Rotear a raiz do projeto
+app.get('/', (req, res) => {
+  // emite uma mensagem com a funcionalidade do servidor
+  res.status(200).send('Servidor API Rest que manipula as rotas /produto e /cliente/.');
+})
+
+// Expor as rotas do servidor
+app.use('/cliente', clienteRotas);
+app.use('/produto', produtoRotas);
+
+
+app.listen(port, hostname, () => {
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
